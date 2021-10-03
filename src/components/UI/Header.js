505443a0979c3d397ad5a88f46ce38a625afb8cd
muruganandham <manosankar2000@gmail.com>
2021-10-03 19:49:17 +0530

    Br-Ba

---
+++ src/components/UI/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../assets/image/logo.png'
+import '../UI/Header.css'
+
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={logo} alt='logo' />
+          
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/UI/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import spinner from '../../assets/image/spinner.gif'
+
+const Spinner = () => {
+    return (
+        <div>
+            <img src={spinner} style={{width: '200px', margin: 'auto' , display: 'block'}} alt='' />
+        </div>
+    )
+}
+
+export default Spinner
